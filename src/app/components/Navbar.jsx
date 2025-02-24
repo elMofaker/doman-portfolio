b458ac6f0229b871157fa01c70cc75ee2d6326ad
elMofaker <155926314+elMofaker@users.noreply.github.com>
2025-02-24 17:47:46 +0200

    d

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
+import Image from "next/image";
 
 const navLinks = [
   {
@@ -45,7 +46,7 @@ const Navbar = () => {
     >
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
-          LOGO
+          {/* <Image src="/images/avatar.svg" alt="img" width={50} height={50}  /> */}
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
